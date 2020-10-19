df62a26911af2749fa5f795c3fcf75c2fed7c106
Alexa Botello <abotello20@yahoo.com>
2020-10-19 07:19:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,7 @@ def get_japanese_emoticon(path, emoticon)
     if value[:english] == emoticon
     return value[:japanese]
   end
+  return "Sorry, that emoticon was not found"
 end 
 
 def get_english_meaning(path, emoticon)
