7d13a36b5a15f9bac973f5ab74f77cb5a75c50ef
Alexa Botello <abotello20@yahoo.com>
2020-10-19 07:15:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,9 +13,7 @@ end
 
 def get_japanese_emoticon(path, emoticon)
   load_library(path).each do |key, value|
-    if value[:english] == emoticon
-      return value[:japanese]
-    end 
+
 end 
 
 def get_english_meaning(path, emoticon)
